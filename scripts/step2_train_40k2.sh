ed3467351c9824361a6ae99ca4c26134c1be2203
sdongchuanqi <57867607+sdongchuanqi@users.noreply.github.com>
2022-10-10 01:15:21 +0800

    Update step2_train_40k2.sh

--- scripts/step2_train_40k2.sh
+++ scripts/step2_train_40k2.sh
@@ -1,23 +1,17 @@
-# 利用提取的对齐进行训练
-# python -m torch.distributed.launch --nproc_per_node=2
-# 输入的文本文件是未bpe， 对齐文件时bpe后的
 
 for type in bert;do
 num=6,7
 layers=8
 epochs=10
-output_dir=/data4/cqdong/my_align/checkpoints_zh_en_dot_bert_1e-5_gold
+output_dir=checkpoint               #checkpoint
 mkdir -p output_dir
-#pretrain_model=/data4/jpzhang/lexicon_induction/pretrain_model/xlm-mlm-xnli15-1024
-#pretrain_model=/data4/cqdong/my_align/xlm-15
-pretrain_model=/data4/jpzhang/lexicon_induction/pretrain_model/bert-base-multilingual-cased
-cache_path=/data4/cqdong/my_align/cache/8w.zhen_gold_${type}\.pt
-# torchrun --nproc_per_node=2     ----simalign \       --learning_rate 1e-5\  --overwrite_cache \ --rdzv_backend=c10d --rdzv_endpoint='127.0.0.1:7891' 
-# CUDA_VISIBLE_DEVICES=$num python \
-CUDA_VISIBLE_DEVICES=$num  torchrun --nnodes=1 --nproc_per_node=2  --rdzv_endpoint='127.0.0.1:7892'\
-     /data4/cqdong/my_align/src/align_train.py\
-    --train_file /data4/cqdong/my_align/zh2en/8w/chen.sents\
-    --train_bpe_gold_file /data4/cqdong/my_align/zh2en/8w/8w.word.align.mbert.zeros\
+pretrain_model=the_place_of_downloaded_pretrainedmodel_of_mbert # https://huggingface.co/bert-base-multilingual-cased
+cache_path=cacheplace_for_next_load
+ 
+CUDA_VISIBLE_DEVICES=$num  torchrun --nnodes=1 --nproc_per_node=2 \
+     ../src/align_train.py\
+    --train_file ../8w/chen.sents\
+    --train_bpe_gold_file ../8w/8w.word.align.mbert.zeros\
     --model_name_or_path $pretrain_model\
     --tokenizer_name $pretrain_model\
     --config_name $pretrain_model\
