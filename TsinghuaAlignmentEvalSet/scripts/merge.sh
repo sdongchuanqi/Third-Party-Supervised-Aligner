e58cfe9f36f66c8e03dc736f0e2f5fb741a6f0eb
sdongchuanqi <2216801542@qq.com>
2022-10-10 01:03:37 +0800

    rm data

--- TsinghuaAlignmentEvalSet/scripts/merge.sh
+++
@@ -1,8 +0,0 @@
-out_sents=/data4/jpzhang/lexicon_induction/TsinghuaAlignmentEvalSet/v2/ch2en.sents
-python /data4/jpzhang/lexicon_induction/TsinghuaAlignmentEvalSet/scripts/merge_para_for_align.py\
- --src_file /data4/jpzhang/lexicon_induction/TsinghuaAlignmentEvalSet/v2/chinese\
- --tgt_file /data4/jpzhang/lexicon_induction/TsinghuaAlignmentEvalSet/v2/english\
- --truc \
- --min_len 5\
- --max_len 128\
- --out_file $out_sents
